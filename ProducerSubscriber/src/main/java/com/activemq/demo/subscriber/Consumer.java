a39c1cae85e8c8b0a2f20f4f7ab2d1e0547d8e09
ankittyagi721 <ankit.tyagi@rsystems.com>
2021-09-15 19:36:25 +0530

    producer consumer using activemq

---
+++ ProducerSubscriber/src/main/java/com/activemq/demo/subscriber/Consumer.java
@@ -0,0 +1,12 @@
+package com.activemq.demo.subscriber;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "standalone.queue")
+    public void consume(String message) {
+        System.out.println("Received Message: " + message);
+    }
+}
